0e8fb6af7881dec6f0c39ef0fbe2b4f8863fa576
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-10-07 12:41:19 +0300

    update

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -2,13 +2,13 @@ import { createReducer, combineReducers } from '@reduxjs/toolkit';
 import { addContact, removeContact, changeFilter } from './contacts-actions';
 
 const items = createReducer([], {
-  [addContact.type]: (state, action) => [...state, action.payload],
-  [removeContact.type]: (state, action) =>
+  [addContact]: (state, action) => [...state, action.payload],
+  [removeContact]: (state, action) =>
     state.filter(item => item.id !== action.payload),
 });
 
 const filter = createReducer('', {
-  [changeFilter.type]: (state, action) => action.payload,
+  [changeFilter]: (state, action) => action.payload,
 });
 
 const contactsReducer = combineReducers({ items, filter });
